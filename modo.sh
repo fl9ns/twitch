e348959cc0fa2b181be4d0f007aaff70eb23b5ee
Flens <95050333+fl9ns@users.noreply.github.com>
2023-08-13 06:22:53 +0200

    1.0.0

---
+++ modo.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+screen -d -m firefox -p "Twitch" -height 1080 -width 800 --new-window https://www.twitch.tv/popout/moderator/farah/activity-feed
+screen -d -m firefox -p "Twitch" -height 1080 -width 800 --new-window https://www.twitch.tv/popout/moderator/farah/reward-queue
